2c70cfdf2b5b800e36981db08e32323e358727cc
Sacha-Lewis Dmytruk <sachalewis@undologic.com>
2025-02-28 16:58:45 -0500

    Added alternate version with scripts to re-establish connection

---
+++ 2025-02-28/2loginDockerContainer.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+docker exec -it file-listing-container-2025-02-28 sh
---
+++ 2025-02-28/reEstablishGoogleDriveConnection.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+echo -------------------------
+
+echo Restarting Docker Desktop
+timeout /t 5
+wsl --shutdown
+
+
